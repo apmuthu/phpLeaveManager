b5ed966e7521cb47468dff34f78d84dba43d1367
Ap.Muthu <apmuthu@usa.net>
2016-06-20 20:30:47 +0530

    TCPDF v6.2.12 with basic fonts for PDF Output uploaded https://github.com/tecnickcom/TCPDF/archive/6.2.12.tar.gz

---
+++ webroot/lang/tam.php
@@ -0,0 +1,44 @@
+<?php
+//============================================================+
+// File name   : tam.php
+// Begin       : 2004-03-03
+// Last Update : 2010-10-26
+//
+// Description : Language module for TCPDF
+//               (contains translated texts)
+//               Tamil
+//
+// Author: Ap.Muthu
+//
+// (c) Copyright:
+//               Ap.Muthu
+//               apmuthu.com
+//               www.apmuthu.com
+//               apmuthu@usa.net
+//============================================================+
+
+/**
+ * TCPDF language file (contains translated texts).
+ * @package com.tecnick.tcpdf
+ * @brief TCPDF language file: Tamil
+ * @author Ap.Muthu
+ * @since 2016-06-09
+ */
+
+// Tamil
+
+global $l;
+$l = Array();
+
+// PAGE META DESCRIPTORS --------------------------------------
+
+$l['a_meta_charset'] = 'UTF-8';
+$l['a_meta_dir'] = 'ltr';
+$l['a_meta_language'] = 'ta';
+
+// TRANSLATIONS --------------------------------------
+$l['w_page'] = 'பக்கம்';
+
+//============================================================+
+// END OF FILE
+//============================================================+
